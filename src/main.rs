dec6165cc49f8f2c5e5cfa72f895a0898eb35a35
xxorza <1442523+xorza@users.noreply.github.com>
2024-05-04 20:39:21 +0300

    Refactor decode_bencoded_value function for error handling
    The decode_bencoded_value function in src/main.rs has been refactored. It now returns a Result type to better handle potential errors. The main function has also been adjusted to utilize the improved error handling, with printing the decoded value now included.

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,39 @@ use std::env;
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+    let mut value_to_decode = encoded_value;
+    // let json: serde_json:: = serde_json::Value::Null;
+
+    while value_to_decode.len() >= 2 {
+        let first_char = value_to_decode.chars().nth(0).unwrap();
+
+
+        match first_char {
+            'i' => {}
+            'l' => {}
+            '0'..='9' => {
+                let (len, rest) = value_to_decode.split_once(':')
+                    .ok_or("Invalid string format")?;
+                let len = len.parse::<usize>()?;
+                if rest.len() < len {
+                    return Err("Invalid string format".into());
+                }
+                let (value, rest) = rest.split_at(len);
+                value_to_decode = rest;
+
+                return Ok(value.into());
+            }
+
+            _ => {}
+        }
     }
+
+    Ok(serde_json::Value::Null)
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
@@ -29,10 +46,12 @@ fn main() {
         println!("Logs from your program will appear here!");
 
         // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value)?;
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
+
+    Ok(())
 }
